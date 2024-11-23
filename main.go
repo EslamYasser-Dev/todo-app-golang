933e8bbfac1cf53cefda0e5d5392a1dc781e5780
eslam <e.yasser997@gmail.com>
2024-11-23 21:51:18 +0200

    impelementing patch, delete routes, load dotenv files

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
 
 type Note struct {
@@ -17,6 +19,12 @@ type Note struct {
 func main() {
 	fmt.Println("hello ")
 	app := fiber.New()
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Failed to load env file")
+	}
+
+	PORT := os.Getenv("PORT")
 	notes := []Note{}
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -25,24 +33,39 @@ func main() {
 
 	app.Post("/api/notes", func(c *fiber.Ctx) error {
 		note := &Note{}
-
 		if err := c.BodyParser(note); err != nil {
 			return err
 		}
-
 		if note.Content == "" {
 			return c.Status(400).JSON(fiber.Map{"error": "content is required"})
 		}
-
 		note.ID = uuid.NewString()
 		notes = append(notes, *note)
-		return c.Status(201).JSON(note)
+		return c.Status(201).JSON(fiber.Map{"status": "success", "data": note})
 	})
 
-	app.Patch("/api/notes:id", func(c *fiber.Ctx) error {
-		return nil
+	app.Patch("/api/notes/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		for i, note := range notes {
+			if fmt.Sprint(note.ID) == id {
+				notes[i].Done = true
+				return c.Status(200).JSON(notes[i])
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{"error": "not found"})
 	})
 
-	log.Fatal(app.Listen(":4000"))
+	app.Delete("/api/notes/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		for i, note := range notes {
+			if fmt.Sprint(note.ID) == id {
+				notes = append(notes[:i], notes[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"status": "success"})
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{"error": "not found"})
+	})
+
+	log.Fatal(app.Listen(":" + PORT))
 
 }
